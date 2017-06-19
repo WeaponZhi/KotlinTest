f32717fc2fa1ad758095474857b430b4ec905a07
WeaponZhi <584098488@qq.com>
2017-06-19 15:52:33 +0800

    kotlin 配置和一些基础界面的绘制

---
+++ app/src/main/java/com/sojex/weaponzhi/kotlintest/adapter/ForecastListAdapter.kt
@@ -0,0 +1,25 @@
+package com.sojex.weaponzhi.kotlintest.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * ForecastListAdapter RecyclerView 的 Adapter
+ * author:张冠之
+ * time: 2017/6/19 15:37
+ * e-mail: guanzhi.zhang@sojex.cn
+ */
+class ForecastListAdapter(val items:List<String>):RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        return ViewHolder(TextView(parent.context))
+    }
+
+    class ViewHolder(val textView : TextView):RecyclerView.ViewHolder(textView)
+}

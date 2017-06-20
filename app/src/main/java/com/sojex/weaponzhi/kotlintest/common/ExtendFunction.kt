013d24c20e86dcf693a85f431977b9e8fd1acca0
WeaponZhi <584098488@qq.com>
2017-06-20 18:11:55 +0800

    添加版本控制执行内联函数

--- app/src/main/java/com/sojex/weaponzhi/kotlintest/common/ExtendFunction.kt
+++ app/src/main/java/com/sojex/weaponzhi/kotlintest/common/ExtendFunction.kt
@@ -1,6 +1,7 @@
 package com.sojex.weaponzhi.kotlintest.common
 
 import android.content.Context
+import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -18,4 +19,24 @@ import android.widget.Toast
 fun Context.toast(message:String, duration: Int = Toast.LENGTH_SHORT){
     Toast.makeText(this,message,duration).show()
 }
+
+/**
+ * 重载 ViewGroup[] 操作符
+ */
 operator fun ViewGroup.get(position:Int): View = getChildAt(position)
+
+/**
+ * 内联函数：检查版本，如果版本大于 lollipop 或者更高版本才能执行
+ *
+ *   supportsLollipop{
+ *
+ *       window.setStatusBarColor(Color.BLACK)
+ *
+ *   }
+ *
+ */
+inline fun supportsLollipop(code:()->Unit){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+        code()
+    }
+}

65cb440a2131aabf361105c63efc94604f03a7bf
WeaponZhi <584098488@qq.com>
2017-06-21 10:43:04 +0800

    代码润色

--- app/src/main/java/com/sojex/weaponzhi/kotlintest/web/RequestForecastCommand.kt
+++ app/src/main/java/com/sojex/weaponzhi/kotlintest/web/RequestForecastCommand.kt
@@ -7,7 +7,7 @@ import com.sojex.weaponzhi.kotlintest.common.domain.ForecastList
 /**
  * Created by admin on 2017/6/20.
  */
-class RequestForecastCommand (val zipCode:String):Command<ForecastList>{
+class RequestForecastCommand (private val zipCode:String):Command<ForecastList>{
     override fun execute(): ForecastList {
         val forecastRequest = ForecastRequest(zipCode)
         return ForecastDataMapper().convertFromDataModel(forecastRequest.execute())

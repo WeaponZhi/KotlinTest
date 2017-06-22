6063792ba749603a9f9f3990a07c7d487330bfe8
WeaponZhi <584098488@qq.com>
2017-06-22 10:28:05 +0800

    数据库

---
+++ app/src/main/java/com/sojex/weaponzhi/kotlintest/database/table/CityForecastTable.kt
@@ -0,0 +1,11 @@
+package com.sojex.weaponzhi.kotlintest.database.table
+
+/**
+ * Created by admin on 2017/6/22.
+ */
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
---
+++ app/src/main/java/com/sojex/weaponzhi/kotlintest/database/table/DayForecastTable.kt
@@ -0,0 +1,15 @@
+package com.sojex.weaponzhi.kotlintest.database.table
+
+/**
+ * Created by admin on 2017/6/22.
+ */
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}

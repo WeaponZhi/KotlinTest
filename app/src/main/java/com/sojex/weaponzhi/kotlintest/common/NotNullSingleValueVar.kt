17d2005218a02817dc80f803d8bf3a8b3dbaaade
WeaponZhi <584098488@qq.com>
2017-06-23 18:17:28 +0800

    泛型 Preferences 委托

--- app/src/main/java/com/sojex/weaponzhi/kotlintest/common/NotNullSingleValueVar.kt
+++ app/src/main/java/com/sojex/weaponzhi/kotlintest/common/NotNullSingleValueVar.kt
@@ -1,5 +1,6 @@
 package com.sojex.weaponzhi.kotlintest.common
 
+import android.content.Context
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -9,18 +10,63 @@ import kotlin.reflect.KProperty
  * time: 2017/6/21 17:24
  * e-mail: guanzhi.zhang@sojex.cn
  */
-private class NotNullSingleValueVar<T> : ReadWriteProperty<Any?,T>{
+private class NotNullSingleValueVar<T> : ReadWriteProperty<Any?, T> {
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
         this.value = if (this.value == null) value
         else throw IllegalStateException("already initialized")
     }
 
-    private var value : T? = null
+    private var value: T? = null
     override fun getValue(thisRef: Any?, property: KProperty<*>): T {
         return value ?: throw IllegalStateException("not initialized")
     }
 
 }
-object DelegatesExt{
-    fun <T> notNullSingleValue():ReadWriteProperty<Any?,T> = NotNullSingleValueVar()
+
+/**
+ * 泛型 preference 委托
+ */
+class Preference<T>(val context: Context, val name: String, val default: T)
+    : ReadWriteProperty<Any?, T> {
+
+    val prefs by lazy { context.getSharedPreferences("default", Context.MODE_PRIVATE) }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        putPreference(name,value)
+    }
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return findPreference(name,default)
+    }
+
+    private fun <T> findPreference(name:String,default:T):T = with(prefs){
+        val res:Any = when(default){
+            is Long -> getLong(name,default)
+            is String -> getString(name,default)
+            is Int -> getInt(name,default)
+            is Boolean -> getBoolean(name,default)
+            is Float -> getFloat(name,default)
+            else -> throw IllegalArgumentException("This type can't be saved into Preferences")
+        }
+        res as T
+    }
+
+    private fun <U> putPreference(name:String ,value: U) = with(prefs.edit()){
+        when(value){
+            is Long -> putLong(name,value)
+            is String -> putString(name,value)
+            is Int -> putInt(name,value)
+            is Boolean -> putBoolean(name,value)
+            is Float -> putFloat(name,value)
+            else -> throw IllegalArgumentException("This type can't be saved into Preferences")
+        }.apply()
+    }
+
+}
+
+object DelegatesExt {
+
+    fun <T> notNullSingleValue(): ReadWriteProperty<Any?, T> = NotNullSingleValueVar()
+
+    fun <T:Any> preference(context: Context, name: String, default:T) = Preference(context,name,default)
 }
